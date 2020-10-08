491460e70cfcbb9075160999b92561e026496c09
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2020-10-08 21:13:19 +0300

    EPMDJ-4840: Add tooltip into overflow-text component
    Issue: EPMDJ-4840

--- src/components/overflow-text/overflow-text.stories.tsx
+++ src/components/overflow-text/overflow-text.stories.tsx
@@ -4,7 +4,20 @@ import { storiesOf } from '@storybook/react';
 import { OverflowText } from './overflow-text';
 
 storiesOf('Overflowext', module).add('Overflow text', () => (
-  <div style={{ width: '150px' }}>
-    <OverflowText>Overflow text Overflow text</OverflowText>
+  <div style={{ padding: 10 }}>
+    <div style={{ width: '150px' }}>
+      <OverflowText>Overflow text Overflow text</OverflowText>
+    </div>
+    <div style={{ width: '70%' }}>
+      <OverflowText>
+        Overflow text Overflow text Overflow text Overflow text
+        Overflow text Overflow text Overflow text Overflow text
+        Overflow text Overflow text Overflow text Overflow text
+        Overflow text Overflow text Overflow text Overflow text
+      </OverflowText>
+    </div>
+    <div style={{ width: '300px' }}>
+      <OverflowText>No Overflow text </OverflowText>
+    </div>
   </div>
 ));
--- src/components/overflow-text/overflow-text.tsx
+++ src/components/overflow-text/overflow-text.tsx
@@ -1,5 +1,42 @@
+import * as React from 'react';
 import { BEM } from '@redneckz/react-bem-helper';
 
 import styles from './overflow-text.module.scss';
 
-export const OverflowText = BEM(styles)('div');
+export interface Props {
+  className?: string;
+  children: string;
+}
+
+const overflowText = BEM(styles);
+
+export const OverflowText = overflowText(({ className, children }: Props) => {
+  const messageRef = React.useRef<HTMLDivElement>(null);
+  const [hint, setHint] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => (!entry.isIntersecting ? setHint(children) : setHint(null)),
+      {
+        root: null,
+        threshold: 1.0,
+      },
+    );
+
+    messageRef.current && observer.observe(messageRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className={className} data-title={hint}>
+      <Wrapper>
+        <span ref={messageRef}>{children}</span>
+      </Wrapper>
+    </div>
+  );
+});
+
+const Wrapper = overflowText.wrapper('span');

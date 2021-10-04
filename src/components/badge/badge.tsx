fc1b0936e7bc21a52b13db5e6719f0cd3e6ce6c2
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-10-04 16:26:42 +0300

    feat: update badge and spinner

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -7,15 +7,15 @@ interface Props {
 }
 
 export const Badge = styled.span<Props>`
-  padding: 0 8px;
+  padding: 0 4px;
   border: 1px solid currentColor;
   border-radius: 11px;
-  font-size: 12px;
-  line-height: 20px;
+  font-size: 8px;
+  line-height: 12px;
   ${({ bold }) => bold && tw`font-bold`}
   ${({ color }) => [
     color === 'gray' && tw`text-monochrome-default`,
-    color === 'green' && tw`text-green-default`,
+    color === 'green' && tw`text-green-medium-tint`,
     color === 'orange' && tw`text-orange-default`,
     color === 'red' && tw`text-red-default`,
   ]}

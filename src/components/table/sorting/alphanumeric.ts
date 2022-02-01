e9343f405886ff7106875290b460e97245ab26c1
ilia_solovev1 <ilia_solovev1@epam.com>
2022-02-01 16:25:34 +0300

    fix(alphanumeric sorting): latin  < Cyrillic

--- src/components/table/sorting/alphanumeric.ts
+++ src/components/table/sorting/alphanumeric.ts
@@ -2,14 +2,14 @@ import { Row } from 'react-table';
 
 export const alphanumeric = (rowA: Row, rowB: Row, columnId: string, desc: boolean) => {
   let [a, b] = getRowValuesByColumnID(rowA, rowB, columnId);
-  a = a.toString();
-  b = b.toString();
+  a = a?.toString();
+  b = b?.toString();
 
-  if (a.toString() === b.toString()) { // no sort if values is equal
+  if (a?.toLowerCase() === b?.toLowerCase()) { // no sort if values is equal
     return desc ? -1 : 1;
   }
 
-  return a.localeCompare(b, undefined, {
+  return a?.localeCompare(b, ['en-US', 'ru-RU'], {
     numeric: true,
     sensitivity: 'base',
   });

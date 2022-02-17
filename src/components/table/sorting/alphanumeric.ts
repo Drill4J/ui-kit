3df64e0d86b8dd40b1c4cadf6ea0d81da16ff471
ilia_solovev1 <ilia_solovev1@epam.com>
2022-02-17 16:11:59 +0300

    feat(alphanumeric sort): add guard when values is undefined

--- src/components/table/sorting/alphanumeric.ts
+++ src/components/table/sorting/alphanumeric.ts
@@ -2,14 +2,14 @@ import { Row } from 'react-table';
 
 export const alphanumeric = (rowA: Row, rowB: Row, columnId: string, desc: boolean) => {
   let [a, b] = getRowValuesByColumnID(rowA, rowB, columnId);
-  a = a.toString();
-  b = b.toString();
+  a = a?.toString();
+  b = b?.toString();
 
-  if (a.toString() === b.toString()) { // no sort if values is equal
+  if (a === b) { // no sort if values is equal
     return desc ? -1 : 1;
   }
 
-  return a.localeCompare(b, undefined, {
+  return a?.localeCompare(b, undefined, {
     numeric: true,
     sensitivity: 'base',
   });

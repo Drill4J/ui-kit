b6d15ac61224e0c25e55035d3d7eb38a77bec245
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-02-28 15:07:35 +0300

    fix: padding in disabled form group

--- src/components/forms/disabled-form-group/disabled-form-group.tsx
+++ src/components/forms/disabled-form-group/disabled-form-group.tsx
@@ -14,7 +14,7 @@ export const DisabledFormGroup = ({ fields }: Props) => (
   <div tw="space-y-6 p-6 border border-monochrome-dark rounded text-14 leading-20">
     {fields.map(({ label, name }) => (
       <div key={name}>
-        <div tw="text-monochrome-medium-tint font-bold">{label}</div>
+        <div tw="mb-2 text-monochrome-medium-tint font-bold">{label}</div>
         <Field name={name}>
           {({ field }: any) => <div tw="text-14 text-monochrome-dark-tint">{field?.value}</div>}
         </Field>

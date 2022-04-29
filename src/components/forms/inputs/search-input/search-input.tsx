63d521571436f8f24dda21310f200c0ec2bb33c4
dmitrii-kliushentsev <94057436+dmitrii-kliushentsev@users.noreply.github.com>
2022-04-29 09:11:22 +0300

    Fix autocomplete (#230)
    * feat: truncated input
    
    * bump: version
    
    * fix: sort options and reset search
    
    * bump: version

--- src/components/forms/inputs/search-input/search-input.tsx
+++ src/components/forms/inputs/search-input/search-input.tsx
@@ -22,7 +22,11 @@ export const SearchInput = ({
   return (
     <div tw="w-full relative">
       <div tw="flex h-6 items-center" className={className}>
-        <SearchIcon onClick={() => setIsOpen(!isOpen)} active={isOpen || restProps?.value} data-test="search-input:enable-input" />
+        <SearchIcon
+          onClick={reset}
+          active={isOpen || restProps?.value}
+          data-test="search-input:enable-input"
+        />
         {isOpen && <Input {...restProps} ref={node} />}
         {isOpen && restProps?.value && (
           <ClearIcon width={8} height={8} onClick={reset} data-test="search-input:clear-icon" />

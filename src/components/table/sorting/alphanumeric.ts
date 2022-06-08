68c5013e4ade8d72066b2907fa09923c058b4afe
dmitrii-kliushentsev <94057436+dmitrii-kliushentsev@users.noreply.github.com>
2022-06-08 21:57:33 +0300

    fix: sorting for number collums (#234)
    * fix: sorting for number collums
    
    * bump: version

--- src/components/table/sorting/alphanumeric.ts
+++ src/components/table/sorting/alphanumeric.ts
@@ -2,6 +2,7 @@ import { Row } from 'react-table';
 
 export const alphanumeric = (rowA: Row, rowB: Row, columnId: string, desc: boolean) => {
   let [a, b] = getRowValuesByColumnID(rowA, rowB, columnId);
+
   a = a?.toString();
   b = b?.toString();
 
@@ -9,10 +10,13 @@ export const alphanumeric = (rowA: Row, rowB: Row, columnId: string, desc: boole
     return desc ? -1 : 1;
   }
 
-  return a?.localeCompare(b, ['en-US', 'ru-RU'], {
-    numeric: true,
-    sensitivity: 'base',
-  });
+  return Intl.Collator(
+    ['en-US', 'ru-RU'],
+    {
+      numeric: true,
+      sensitivity: 'base',
+    },
+  ).compare(a, b);
 };
 
 function getRowValuesByColumnID(row1: Row, row2: Row, columnId: string) {
